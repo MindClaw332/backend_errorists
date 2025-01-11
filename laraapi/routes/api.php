ec400bee4d68eb91aee3015fbf3f0ba13e290fd2
Michael_Schrijvers_Thuis <michael.10330@hotmail.com>
2025-01-11 21:03:47 +0100

    reformatted users endpoint and fixed post route

--- laraapi/routes/api.php
+++ laraapi/routes/api.php
@@ -2,11 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 // Courses endpoint
 // Get all courses
 Route::get('/courses', function () {
-    $course = DB::select('SELECT name FROM courses');
+    $course = DB::select('SELECT * FROM courses
+                        ORDER BY name ASC');
     return response()->json($course);
 });
 
@@ -22,7 +24,8 @@ Route::get('/courses/{id}', function ($id) {
 // Create a new course
 Route::post('/courses', function (\Illuminate\Http\Request $request) {
     $name = $request->input('name');
-    DB::insert('INSERT INTO courses (name) VALUES (?)', [$name]);
+    DB::insert('INSERT INTO courses (name) 
+                VALUES (?)', [$name]);
     return response()->json(['message' => 'course created successfully'], 201);
 });
 
@@ -33,4 +36,55 @@ Route::delete('/courses/{id}', function ($id) {
         return response()->json(['message' => 'course not found'], 404);
     }
     return response()->json(['message' => 'course deleted successfully']);
+});
+
+// Users endpoint
+// Get all users
+Route::get('/users', function () {
+    $user = DB::select('SELECT 
+                        users.id, firstname, lastname, email, password, eligible, roles.name AS role, classes.name AS class, year AS schoolyear
+                        FROM users
+                        LEFT JOIN roles ON roles.id = users.role_id
+                        LEFT JOIN classes ON classes.id = users.class_id
+                        ORDER BY users.firstname ASC');
+    return response()->json($user);
+});
+
+// Get user by id
+Route::get('/users/{id}', function ($id) {
+    $user = DB::select('SELECT 
+                        firstname, lastname, email, password, eligible, roles.name AS role, classes.name AS class, year AS schoolyear
+                        FROM users
+                        INNER JOIN roles ON roles.id = users.role_id
+                        INNER JOIN classes ON classes.id = users.class_id
+                        WHERE users.id = ?', [$id]);
+    if (empty($user)) {
+        return response()->json(['message' => 'user not found'], 404);
+    }
+    return response()->json($user[0]);
+});
+
+// Create a new user
+Route::post('/users', function (\Illuminate\Http\Request $request) {
+    $firstname = $request->input('firstname');
+    $lastname = $request->input('lastname');
+    $email = $request->input('email');
+    $password = $request->input('password');
+    $eligible = $request->input('eligible');
+    $role_id = $request->input('role_id');
+    $class_id = $request->input('class_id');
+
+    DB::insert('INSERT INTO users (firstname, lastname, email, password, eligible, role_id, class_id) 
+                VALUES (?, ?, ?, ?, ?, ?, ?)', [$firstname, $lastname, $email, $password, $eligible, $role_id, $class_id]);
+
+    return response()->json(['message' => 'user created successfully'], 201);
+});
+
+// Delete a user by ID
+Route::delete('/users/{id}', function ($id) {
+    $deleted = DB::delete('DELETE FROM users WHERE id = ?', [$id]);
+    if ($deleted === 0) {
+        return response()->json(['message' => 'user not found'], 404);
+    }
+    return response()->json(['message' => 'user deleted successfully']);
 });
